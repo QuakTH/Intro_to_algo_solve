c7c1b786febebd1c61d3f15f24e24519808ef68c
Quack <bcde59@gmail.com>
2023-05-06 15:26:26 +0900

    added share and remainder attributes to Node for radix sort

--- intro_to_algo_solve/lecture7/node.py
+++ intro_to_algo_solve/lecture7/node.py
@@ -10,7 +10,9 @@ class Node:
         :param key: Key of the node. (Used for sorting)
         :param value: Value which the node holds. Could be any value.
         """
-        assert isinstance(key, int), 'A `key` must be a Integer type.'
+        assert isinstance(key, int), "A `key` must be a Integer type."
 
         self.key = key
         self.value = value
+        self.share = key  # a value for doing radix sort
+        self.remainder = 0  # a value for doing radix sort
