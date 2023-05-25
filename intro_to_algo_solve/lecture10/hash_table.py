495607eeaa583af0e63b9e0fe75a2b0675e1e7e2
Quack <bcde59@gmail.com>
2023-05-26 02:36:20 +0900

    created hash table codes

--- intro_to_algo_solve/lecture10/hash_table.py
+++ intro_to_algo_solve/lecture10/hash_table.py
@@ -5,7 +5,7 @@
 # 2. The Node will be assigned to the table with the unique index. (No linked list)
 
 import random
-from typing import Any
+from typing import Any, Optional
 
 from intro_to_algo_solve.lecture8 import hash_table
 from intro_to_algo_solve.lecture8.prime_generator import generate_large_prime
@@ -25,6 +25,7 @@ class Node:
         """
         self.key = key
         self.value = value
+        self.deleted = False  # Delete flag
 
 
 class HashTable(hash_table.HashTable):
@@ -48,3 +49,131 @@ class HashTable(hash_table.HashTable):
         assert (
             self.big_prime_num2 > self.table_length
         ), "A big prime number must be larger than the table length."
+
+    def hash_key(self, key: str, multiplier: int) -> int:
+        """Hash the key using the Universal Hashing.
+        Do a double hashing probing strategy.
+
+        :param key: Key to hash.
+        :param multiplier: Value to multiply to second hashed value.
+        :return: A hashed key.
+        """
+        hashed_key = hash(key)
+        h1 = (self.a * hashed_key + self.b) % self.big_prime_num
+        h2 = (self.a2 * hashed_key + self.b2) % self.big_prime_num2
+
+        return (h1 + multiplier * h2) % self.table_length
+
+    def search(self, key: str) -> Optional[Node]:
+        """Search for the node with the `key` as a key.
+        If not found, return None.
+
+        :param key: Key of a node to search.
+        :return: The node itself or None if not found.
+        """
+        for multiplier in range(self.table_length):
+            hashed_val = self.hash_key(key, multiplier)
+            val_at_idx = self.table[hashed_val]
+
+            if val_at_idx is None or val_at_idx.key != key:
+                continue
+
+            # if the found node was deleted break the loop
+            if val_at_idx.deleted:
+                break
+
+            return val_at_idx
+        return None
+
+    def create_new_table(self, reduce=False) -> None:
+        """Do a table doubling if reduce is False, else do table halfing.
+
+        :param reduce: Whether to do table doubling or halfing, defaults to False.
+        """
+        assert (
+            self.big_prime_num > self.table_length * 2
+            and self.big_prime_num2 > self.table_length * 2
+        ), "A big prime number must be larger than the new table length."
+
+        old_table = self.table
+        self.table = (
+            [None for _ in range(self.table_length // 2)]
+            if reduce
+            else [None for _ in range(self.table_length * 2)]
+        )
+        self.table_length = len(self.table)
+
+        for element in old_table:
+            if element is None or element.deleted:
+                continue
+
+            for multiplier in range(self.table_length):
+                hashed_val = self.hash_key(element.key, multiplier)
+                val_at_idx = self.table[hashed_val]
+
+                if val_at_idx is None:
+                    self.table[hashed_val] = element
+                    break
+
+    def insert(self, key: str, value: Any) -> None:
+        """Insert a `value` with a specific `key`.
+        Do a table doubling when currently inserted elements num + 1 == table length.
+        If there is a node with the same key, change the value to `value`.
+
+        :param key: Key for the value.
+        :param value: Value to insert.
+        """
+        # if there is a node with the same key change the value
+        search_result = self.search(key)
+        if search_result:
+            search_result.value = value
+            return
+
+        for multiplier in range(self.table_length):
+            hashed_val = self.hash_key(key, multiplier)
+            val_at_idx = self.table[hashed_val]
+
+            if val_at_idx is None:
+                self.table[hashed_val] = Node(key, value)
+                break
+
+            if val_at_idx.deleted:
+                val_at_idx.key = key
+                val_at_idx.value = value
+                val_at_idx.deleted = False
+                break
+        else:
+            raise Exception("Table full")
+
+        self.inserted_element += 1
+
+        if self.inserted_element == self.table_length:
+            self.create_new_table()
+
+    def delete(self, key: str) -> None:
+        """Delete the node with the key if it exists.
+        Do a table halfing if the current inserted element number
+        is less or equal to the table length / 4.
+
+        :param key: Key to delete.
+        """
+        search_result = self.search(key)
+
+        if not search_result:
+            print(f"There is no key found : {key}")
+            return
+
+        search_result.deleted = True
+        self.inserted_element -= 1
+
+        if self.inserted_element <= self.table_length / 4:
+            self.create_new_table(True)
+
+    def print_hash_table(self) -> None:
+        """Print the hash table with this format.
+        [index] -> (key, value)
+        """
+        for idx, element in enumerate(self.table):
+            item_string = f"({element.key}, {element.value})" if element else "Empty"
+
+            print(f"[{idx}] -> {item_string}")
