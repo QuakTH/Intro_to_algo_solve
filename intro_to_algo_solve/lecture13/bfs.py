5ff0e45f8f160a763d9f10d80afa5bf8b6c7e863
Quack <bcde59@gmail.com>
2023-07-12 22:19:59 +0900

    created answer to lecture13

---
+++ intro_to_algo_solve/lecture13/bfs.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, Tuple
+from intro_to_algo_solve.lecture13.graph import Graph
+
+
+def breadth_first_search(
+    graph: Graph, start_node: Any
+) -> Tuple[Dict[Any, int], Dict[Any, Any]]:
+    """Do a breadth-first-search for the `graph` starting from `start_node`.
+    And return how many path does it takes to go to each vertex
+    and each vertex's before vertex.
+
+    :param graph: Graph to do bfs.
+    :param start_node: Start node when doing bfs.
+    :return: A tuple which first element is a dictionary of path count,
+             and the second element is the dictionary of before node.
+    """
+    assert (
+        start_node in graph.adjacency_info
+    ), "Start node not in graph's adjacency information."
+
+    shortest_paths = {start_node: 0}
+    before_node = {start_node: None}
+    frontier = [start_node]
+
+    while frontier:
+        next_frontier = []
+        for node in frontier:
+            for neighbor in graph.adjacency_info[node]:
+                if neighbor not in shortest_paths:
+                    shortest_paths[neighbor] = shortest_paths[node] + 1
+                    before_node[neighbor] = node
+                    next_frontier.append(neighbor)
+        frontier = next_frontier
+
+    return shortest_paths, before_node
---
+++ intro_to_algo_solve/lecture13/graph.py
@@ -0,0 +1,33 @@
+from typing import Any, Dict, List
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+class Graph:
+    """A class containing information of the vertices and edges of a undirected graph."""
+
+    def __init__(self, adjacency_info: Dict[Any, List[Any]]) -> None:
+        """Constructor for the Graph instance.
+
+        :param adjacency_info: Adjacency info using a Python dictionary.
+                               Which keys are vertices and values are list of vertices
+                               adjacent to a particular vertex
+        """
+        self.adjacency_info = adjacency_info
+
+    def visualize(self) -> None:
+        """Visualize the graph using the networkx and matplotlib library."""
+        graph = nx.Graph()
+
+        graph.add_nodes_from(self.adjacency_info.keys())
+
+        edge_set = set()
+        for vertex, neighbors in self.adjacency_info.items():
+            for neighbor in neighbors:
+                edge_set.add((vertex, neighbor))
+
+        graph.add_edges_from(edge_set)
+
+        nx.draw(graph, with_labels=True, font_weight="bold")
+        plt.show()
