726bfd6b5bf919a956b09ce40d1b6dbfa7c07a2f
Quack <bcde59@gmail.com>
2023-08-05 20:12:46 +0900

    Created answers to lecture16

---
+++ intro_to_algo_solve/lecture16/dijkstra.py
@@ -0,0 +1,99 @@
+# Module for dijkstra algorithm
+
+
+import math
+from typing import Any, Dict, Optional, Set, Tuple, Union
+
+from intro_to_algo_solve.lecture16.weighted_graph import WeightedGraph
+
+
+def relax(
+    from_node: Any,
+    to_node: Any,
+    min_weight_infos: Dict[Any, Union[int, float]],
+    edge_weight_infos: Dict[Tuple[Any, Any], int],
+    before_node_infos: Dict[Any, Any],
+) -> None:
+    """Update the `min_weight_infos` of `to_node` using the information from:
+    1. Min dist information from `from_node`.
+    2. Edge weight from `from_node` to `to_node`.
+    If the original weight of the `to_node` is smaller than
+    <weight of the `from_node`> + <edge weight of `from_node` to `to_node`>
+    no update is made. else, there will be an update.
+
+    Note that the `min_weight_infos` is a dictionary containing weights of nodes
+    with respect to a start node.
+    Meaning with different start nodes there will be different `min_weight_infos`.
+
+    :param from_node: Node that has already found the minimum weight from the start node.
+    :param to_node: Node that needs the minimum weight updated.
+    :param min_weight_infos: Dictionary containing minimum weight from the start node.
+    :param edge_weight_infos: Weight information of edges.
+    :param before_node_infos: Dictionary containing before node
+                              When the weight is currently minimum.
+    """
+    edge = (from_node, to_node)
+    weight_added = min_weight_infos[from_node] + edge_weight_infos[edge]
+    if min_weight_infos[to_node] > weight_added:
+        min_weight_infos[to_node] = weight_added
+        before_node_infos[to_node] = from_node
+
+
+def get_from_node(
+    min_weight_infos: Dict[Any, Union[int, float]], vertices_done: Set[Any]
+) -> Any:
+    """Return the from_node which will be used as a start point to do a `relax` process.
+    When this function is called first, the start vertex will always be returned.
+
+    :param min_weight_infos: Dictionary containing the minimum weight
+                             information from the start vertex.
+    :param vertices_done: Set containing vertices which has already been relaxed.
+    :return: Vertex to be used as a from_node
+    """
+    priority_queue = sorted(min_weight_infos.items(), key=lambda item: item[1])
+    filtered_queue = [
+        vertex_weight[0]
+        for vertex_weight in priority_queue
+        if vertex_weight[0] not in vertices_done
+    ]
+    return filtered_queue[0]
+
+
+def do_dijkstra(
+    graph: WeightedGraph, start_node: Any
+) -> Tuple[Dict[Any, Union[int, float]], Dict[Any, Optional[Any]]]:
+    """Do a Dijkstra path finding on the weighted graph `graph`.
+    Which start node is the `start_node`.
+
+    :param graph: Graph to do dijkstra.
+    :param start_node: Start node of the graph.
+    :return: Tuple containing two elements which first value is the minimum sum of weights
+             of edges from the `start_node` to each vertices. The second value contains the
+             dictionary of before nodes of each end node when the sum of the weight is minimum.
+    """
+    assert start_node in graph.vertices, "The start node is not in the graph."
+    assert all(
+        weight >= 0 for weight in graph.edge_weight_infos.values()
+    ), "Dijkstra can only be applied in with edges bigger than 0."
+
+    min_weight_infos = {vertex: math.inf for vertex in graph.vertices}
+    min_weight_infos[start_node] = 0
+
+    before_node_infos = {vertex: None for vertex in graph.vertices}
+    vertices_done = set()
+    vertices_not_done = set(graph.vertices)
+
+    while vertices_not_done:
+        from_node = get_from_node(min_weight_infos, vertices_done)
+        for neighbor in graph.adjacency_infos[from_node]:
+            relax(
+                from_node,
+                neighbor,
+                min_weight_infos,
+                graph.edge_weight_infos,
+                before_node_infos,
+            )
+        vertices_done.add(from_node)
+        vertices_not_done.remove(from_node)
+
+    return min_weight_infos, before_node_infos
---
+++ intro_to_algo_solve/lecture16/weighted_graph.py
@@ -0,0 +1,93 @@
+# Module for representing the weighted graph
+
+from typing import Any, Dict, List, Tuple
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+class WeightedGraph:
+    """A class containing information of the vertices, edges, and weight of the edges"""
+
+    def __init__(
+        self, graph_info: Dict[Any, List[Tuple[Any, int]]], is_directed: bool = False
+    ) -> None:
+        """Constructor for the `WeightedGraph` instance.
+
+        :param graph_info: Dictionary containing the graph information.
+        Example of the dictionary is this:
+        {
+            <from node> : [(<to node>, <weight>), (<to node>, <weight>), ...]
+        }
+        :param is_directed: Whether the graph is a directed graph, defaults to False
+        """
+        self.vertices = list(graph_info.keys())
+        self.adjacency_infos = {}
+        self.edge_weight_infos = {}
+        self.is_directed = is_directed
+
+        self.generate_attributes(graph_info)
+        self.check_directed_feat()
+
+    def generate_attributes(self, graph_info: Dict[Any, List[Tuple[Any, int]]]) -> None:
+        """Generate information of edges with associated weights using the `graph_info`.
+        And the adjacent information of each vertices.
+
+        :param graph_info: Dictionary containing the graph information.
+        """
+        for s_node, edge_infos in graph_info.items():
+            self.adjacency_infos[s_node] = []
+            for e_node, weight in edge_infos:
+                self.edge_weight_infos[(s_node, e_node)] = weight
+                self.adjacency_infos[s_node].append(e_node)
+
+    def check_directed_feat(self) -> None:
+        """Check if the `adjacency_info` is correct if `is_directed` is false.(Undirected)"""
+        edges = list(self.edge_weight_infos)
+
+        if not self.is_directed:
+            while edges:
+                current = edges[0]
+                reversed = (current[1], current[0])
+                assert (
+                    reversed in edges
+                    and self.edge_weight_infos[current]
+                    == self.edge_weight_infos[reversed]
+                ), "The graph info is incorrect."
+
+                edges.remove(current)
+                edges.remove(reversed)
+
+    def visualize(self) -> None:
+        """Visualize the graph using the networkx and matplotlib library."""
+        graph = nx.DiGraph() if self.is_directed else nx.Graph()
+
+        graph.add_nodes_from(self.vertices)
+
+        for edge, weight in self.edge_weight_infos.items():
+            reversed_edge = (edge[1], edge[0])
+            if not self.is_directed and reversed_edge in graph.edges:
+                continue
+            graph.add_edge(*edge, weight=weight)
+
+        positions = nx.spring_layout(graph)
+        if self.is_directed:
+            nx.draw(
+                graph,
+                positions,
+                with_labels=True,
+                font_weight="bold",
+                arrows=True,
+                arrowstyle="->",
+                arrowsize=15,
+            )
+        else:
+            nx.draw(
+                graph,
+                positions,
+                with_labels=True,
+                font_weight="bold",
+            )
+        edge_labels = nx.get_edge_attributes(graph, "weight")
+        nx.draw_networkx_edge_labels(graph, positions, edge_labels=edge_labels)
+        plt.show()
